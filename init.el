a8764539c61de18e56569f398a437969a3238adb
Naoki Ueda <naueda@microsoft.com>
2020-06-23 20:15:38 +0900

    Update init.el

---
+++ init.el
@@ -0,0 +1,13 @@
+;;;
+;;; Author: Naoki
+;;;
+
+;; 日本語対応
+(set-language-environment 'Japanese)
+
+;; Coding System
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
